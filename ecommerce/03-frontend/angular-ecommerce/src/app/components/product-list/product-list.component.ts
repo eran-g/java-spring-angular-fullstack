e242dbb5b62e3479740d99a66fb4c50e919511e0
DESKTOP-QSMBT87\erang <eran.goldstar@gmail.com>
2021-09-03 18:32:10 +0300

    111

--- ecommerce/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ ecommerce/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -10,9 +10,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductListComponent implements OnInit {
 
-  products: Product[];
-  currentCategoryId: number;
-  searchMode: boolean;
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
+  searchMode: boolean = false;
+
+  // pagination props
+  pageNumber: number = 1;
+  pageSize: number = 10;
+  totalElements: number = 0;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -62,11 +68,25 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId = 1;
     }
 
+    if(this.previousCategoryId != this.currentCategoryId) {
+      this.pageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+    console.log(`===>currentCategoryId: ${this.currentCategoryId}, pageNumber: ${this.pageNumber}`);
+
     // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )    
+
+    this.productService.getProductListPaginate(this.pageNumber-1, this.pageSize, this.currentCategoryId)
+    .subscribe(this.processResult());
+  }
+
+  processResult(){
+    return data => {
+      this.products = data._embedded.products;
+      this.pageNumber = data.page.number + 1;
+      this.pageSize = data.page.size;
+      this.totalElements = data.page.totalElements;
+    }
   }
 }
